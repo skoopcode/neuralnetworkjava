3c061abdd291a6ba8dbce07fdea56400cfde3492
steffexx@gmail.com <steffexx@gmail.com@e962f4e4-1005-cd6f-265e-de1520d4cd3e>
2012-01-16 21:16:37 +0000

    Updated synapse (bijna t zelfde als die andere maar dan met andere benaming) Updated neuron (zijn nog een paar dingen onduidelijk bij mij maar die ga ik morgen bekijken)

--- NeuralNetwork/src/neuralnetwork/Neuron.java
+++ NeuralNetwork/src/neuralnetwork/Neuron.java
@@ -1,18 +1,53 @@
 package neuralnetwork;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import neuralnetwork.activation.ActivationFunction;
 
 /**
  *
- * @author Marcel
+ * @author Stef Dijksman, Marcel Saarloos
  */
 public class Neuron implements Serializable{
 
     private ActivationFunction af;
+    private List<Synapse> inputSynapses;
+    private double error;
             
     public Neuron(ActivationFunction activationFunction) {
+        inputSynapses = new ArrayList<Synapse>();
         af = activationFunction;
+        error = 0;
+    }
+    public void addInputSynapse(Synapse input)
+    {
+        inputSynapses.add(input);
+    }
+    
+    public List<Synapse> getInputSynapses()
+    {
+        return this.inputSynapses;
+    }
+    
+    public double[] getWeights() 
+    {
+        double[] weights = new double[inputSynapses.size()];
+
+        int i = 0;
+        for(Synapse synapse : inputSynapses) {
+            weights[i] = synapse.getWeight();
+            i++;
+        }
+        return weights;
+    }
+    
+    public double getError() {
+        return error;
+    }
+
+    public void setError(double error) {
+        this.error = error;
     }
     
     public void ActivateNeuron(Neuron neuron)
--- NeuralNetwork/src/neuralnetwork/Synapse.java
+++ NeuralNetwork/src/neuralnetwork/Synapse.java
@@ -4,8 +4,24 @@ import java.io.Serializable;
 
 /**
  *
- * @author Marcel
+ * @author Stef Dijksman
  */
 public class Synapse implements Serializable{
+    private Neuron source;
+    private double weight;
     
+    public Neuron getSource()
+    {
+        return source;
+    }
+    
+    public double getWeight()
+    {
+        return weight;
+    }
+    
+    public void setWeight(double weight)
+    {
+        this.weight = weight;
+    }
 }

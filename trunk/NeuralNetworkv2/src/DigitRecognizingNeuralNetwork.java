b3753f8425f421ea69be26584a445deacdcd65c0
mcsaarloos91@gmail.com <mcsaarloos91@gmail.com@e962f4e4-1005-cd6f-265e-de1520d4cd3e>
2012-01-24 15:44:40 +0000

    Created a new version of the neural network, which seems to work

--- trunk/NeuralNetworkv2/src/DigitRecognizingNeuralNetwork.java
+++ trunk/NeuralNetworkv2/src/DigitRecognizingNeuralNetwork.java
@@ -1,3 +1,4 @@
+
 import digit.generator.DigitTrainingDataGenerator;
 
 import java.io.IOException;
@@ -23,75 +24,103 @@ public class DigitRecognizingNeuralNetwork {
 
         NeuralNetwork neuralNetwork = new NeuralNetwork("Digit Recognizing Neural Network");
 
-        Neuron inputBias = new Neuron(new LinearActivationStrategy());
-        inputBias.setOutput(1);
+        NeuralNetwork tempNetwork = neuralNetwork.readSavedNeuralNetworkFile();
 
-        Layer inputLayer = new Layer(null, inputBias);
+        if (tempNetwork == null) {
 
-        for(int i = 0; i < DigitImageReader.ROWS * DigitImageReader.COLUMNS; i++) {
-            Neuron neuron = new Neuron(new SigmoidActivationStrategy());
-            neuron.setOutput(0);
-            inputLayer.addNeuron(neuron);
-        }
+            Neuron inputBias = new Neuron(new LinearActivationStrategy());
+            inputBias.setOutput(1);
 
-        Neuron hiddenBias = new Neuron(new LinearActivationStrategy());
-        hiddenBias.setOutput(1);
+            Layer inputLayer = new Layer(null, inputBias);
 
-        Layer hiddenLayer = new Layer(inputLayer, hiddenBias);
-
-        long numberOfHiddenNeurons = Math.round((2.0 / 3.0) * (DigitImageReader.ROWS * DigitImageReader.COLUMNS) + 10);
-
-        for(int i = 0; i < numberOfHiddenNeurons; i++) {
-            Neuron neuron = new Neuron(new SigmoidActivationStrategy());
-            neuron.setOutput(0);
-            hiddenLayer.addNeuron(neuron);
-        }
-
-        Layer outputLayer = new Layer(hiddenLayer);
-
-        //10 output neurons - 1 for each digit
-        for(int i = 0; i < 10; i++) {
-            Neuron neuron = new Neuron(new SigmoidActivationStrategy());
-            neuron.setOutput(0);
-            outputLayer.addNeuron(neuron);
-        }
-
-        neuralNetwork.addLayer(inputLayer);
-        neuralNetwork.addLayer(hiddenLayer);
-        neuralNetwork.addLayer(outputLayer);
-
-        DigitTrainingDataGenerator trainingDataGenerator = new DigitTrainingDataGenerator(trainingService.loadDigitImages());
-        Backpropagator backpropagator = new Backpropagator(neuralNetwork, 0.1, 0.5);
-        backpropagator.train(trainingDataGenerator, 0.005);
-        neuralNetwork.writeObjectToFile();
-
-        DigitTrainingDataGenerator testDataGenerator = new DigitTrainingDataGenerator(testService.loadDigitImages());
-        TrainingData testData = testDataGenerator.getTrainingData();
-
-        for(int i = 0; i < testData.getInputs().length; i++) {
-            double[] input = testData.getInputs()[i];
-            double[] output = testData.getOutputs()[i];
-
-            int digit = 0;
-            boolean found = false;
-            while(digit < 10 && !found) {
-                found = (output[digit] == 1);
-                digit++;
+            for (int i = 0; i < DigitImageReader.IMAGE_SIZE; i++) {
+                Neuron neuron = new Neuron(new SigmoidActivationStrategy());
+                neuron.setOutput(0);
+                inputLayer.addNeuron(neuron);
             }
 
-            neuralNetwork.setInputs(input);
-            double[] receivedOutput = neuralNetwork.getOutput();
+            Neuron hiddenBias = new Neuron(new LinearActivationStrategy());
+            hiddenBias.setOutput(1);
 
-            double max = receivedOutput[0];
-            double recognizedDigit = 0;
-            for(int j = 0; j < receivedOutput.length; j++) {
-                if(receivedOutput[j] > max) {
-                    max = receivedOutput[j];
-                    recognizedDigit = j;
+            Layer hiddenLayer = new Layer(inputLayer, hiddenBias);
+
+            //long numberOfHiddenNeurons = Math.round((2.0 / 3.0) * (DigitImageReader.IMAGE_SIZE) + 10); //543
+            int numberOfHiddenNeurons = 50;
+
+            for (int i = 0; i < numberOfHiddenNeurons; i++) {
+                Neuron neuron = new Neuron(new SigmoidActivationStrategy());
+                neuron.setOutput(0);
+                hiddenLayer.addNeuron(neuron);
+            }
+
+            Layer outputLayer = new Layer(hiddenLayer);
+
+            //10 output neurons - 1 for each digit
+            for (int i = 0; i < 10; i++) {
+                Neuron neuron = new Neuron(new SigmoidActivationStrategy());
+                neuron.setOutput(0);
+                outputLayer.addNeuron(neuron);
+            }
+
+            neuralNetwork.addLayer(inputLayer);
+            neuralNetwork.addLayer(hiddenLayer);
+            neuralNetwork.addLayer(outputLayer);
+        } else {
+            neuralNetwork = tempNetwork;
+        }
+
+        if (neuralNetwork != null) {
+            DigitTrainingDataGenerator trainingDataGenerator = new DigitTrainingDataGenerator(trainingService.loadDigitImages());
+
+            Backpropagator tempBack = Backpropagator.readSavedObjectFile();
+
+            Backpropagator backpropagator;
+            if (tempBack == null) {
+                backpropagator = new Backpropagator(neuralNetwork, 0.1, 0.5); //neural network, learning rate, momentum
+                backpropagator.setFinished(false);
+            } else {
+                backpropagator = tempBack;
+            }
+
+            if (!backpropagator.getFinished()) {
+                try {
+                    backpropagator.train(trainingDataGenerator, 0.05);
+                } finally {
+                    neuralNetwork.writeObjectToFile();
+                    backpropagator.writeObjectToFile();
                 }
             }
 
-            System.out.println("Recognized " + (digit - 1) + " as " + recognizedDigit + ". Corresponding output value was " + max);
+            if (neuralNetwork != null && backpropagator.getFinished()) {
+                DigitTrainingDataGenerator testDataGenerator = new DigitTrainingDataGenerator(testService.loadDigitImages());
+                TrainingData testData = testDataGenerator.getTrainingData();
+
+                for (int i = 0; i < testData.getInputs().length; i++) {
+                    double[] input = testData.getInputs()[i];
+                    double[] output = testData.getOutputs()[i];
+
+                    int digit = 0;
+                    boolean found = false;
+                    while (digit < 10 && !found) {
+                        found = (output[digit] == 1);
+                        digit++;
+                    }
+
+                    neuralNetwork.setInputs(input);
+                    double[] receivedOutput = neuralNetwork.getOutput();
+
+                    double max = receivedOutput[0];
+                    double recognizedDigit = 0;
+                    for (int j = 0; j < receivedOutput.length; j++) {
+                        if (receivedOutput[j] > max) {
+                            max = receivedOutput[j];
+                            recognizedDigit = j;
+                        }
+                    }
+
+                    System.out.println("Recognized " + (digit - 1) + " as " + recognizedDigit + ". Corresponding output value was " + max);
+                }
+            }
         }
     }
 }
